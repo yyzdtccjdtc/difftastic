c7499d4c91a77e9a43a84a23b89153fb5233dd55
Wilfred <>
2022-01-22 05:42:34 +0000

    Deploying to gh-pages from @ Wilfred/difftastic@b30f203c999777fa25687dbf47f11d459ce82539 🚀

---
+++ rustdoc/difft/sliders/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["distance_between","Return the distance between two syntax nodes, as a tuple of number of lines and number of columns."],["fix_all_sliders",""],["fix_sliders",""],["is_novel_deep",""],["is_unchanged_deep",""],["novel_regions_after_unchanged",""],["novel_regions_before_unchanged",""],["slide_to_next_node",""],["slide_to_prev_node",""]]});
