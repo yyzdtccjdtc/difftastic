1a10c43be19271401a3340e6ec47ef6637fef6c1
Wilfred <>
2022-02-07 00:59:18 +0000

    Deploying to gh-pages from @ Wilfred/difftastic@dd85457b5e44e765724313bce03049249e18c436 🚀

---
+++ rustdoc/difft/options/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["DEFAULT_NODE_LIMIT",""]],"enum":[["ColorOutput",""],["Mode",""]],"fn":[["app",""],["configure_color",""],["detect_display_width","Choose the display width: try to autodetect, or fall back to a sensible default."],["parse_args","Parse CLI arguments passed to the binary."]]});
